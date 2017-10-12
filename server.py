73b508282034e65e4e544d96aee0f26be2c42f57
Dean Petty <dean.petty@gmail.com>
2017-10-11 19:47:45 -0500

    Changing pins output calls

--- server.py
+++ server.py
@@ -24,25 +24,24 @@ def on():
 @app.post('/off')
 def off():
 	print "Turning Lights Off"
-	GPIO.output(8,GPIO.LOW)
-	GPIO.output(10,GPIO.LOW)
-	GPIO.output(12,GPIO.LOW)
-	GPIO.cleanup()
+	GPIO.output(8,False)
+	GPIO.output(10,False)
+	GPIO.output(12,False)
 
 @app.post('/colorWhite')
 def colorWhite():
 	print "Turning light white"
 	# Turn LEDs on
-	GPIO.output(8,GPIO.HIGH)
-	GPIO.output(10,GPIO.HIGH)
-	GPIO.output(12,GPIO.HIGH)
+	GPIO.output(8,True)
+	GPIO.output(10,True)
+	GPIO.output(12,True)
 	sleep(1) # sleep for 1 second
 
 @app.post('/colorRed')
 def colorRed():
 	print "Turning light Red"
 	# Turn LEDs on
-	GPIO.output(8,GPIO.HIGH)
+	GPIO.output(8,True)
 #	GPIO.output(27,GPIO.HIGH)
 	sleep(1) # sleep for 1 second
 
@@ -50,19 +49,22 @@ def colorRed():
 def colorRed():
 	print "Turning light Blue"
 	# Turn LEDs on
-	GPIO.output(10,GPIO.HIGH)
+	GPIO.output(10,True)
 	sleep(1) # sleep for 1 second
 
 @app.post('/colorGreen')
 def colorRed():
 	print "Turning light Green"
 	# Turn LEDs on
-	GPIO.output(12,GPIO.HIGH)
+	GPIO.output(12,True)
 	sleep(1) # sleep for 1 second
 
 @app.post('/shutdown')
 def shutdown():
 	print "Shutdown"
+	GPIO.output(8,False)
+	GPIO.output(10,False)
+	GPIO.output(12,False)
 	GPIO.cleanup() # the clean-up function will reset all the configurations made in this script. This will stop the warnings we got from the tutorial 2.
 
 run(app, host='192.168.0.35', port=8080)	
